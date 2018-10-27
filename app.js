b4df7d996be3d172208b64e3ed0265fc384372fc
perezm.josea <perezm.josea@gmail.com>
2018-10-27 15:03:08 +0200

    Filters & Categories

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const chalk = require("chalk");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 
 const websitesRouter = require("./routes/websites");
-const PORT = process.env.PORT || 3001;
-const DB_NAME = process.env.DB_NAME || "cicewebsites";
+const PORT = process.env.PORT || 3003;
+const DB_NAME = process.env.DB_NAME || "cicepocket";
+
+// const DB_URL = `mongodb://${process.env.DB_URL || "localhost:27017"}`;
 const DB_URL = `mongodb://${process.env.DB_URL || "localhost:27018"}`;
 
 const app = express();
@@ -36,12 +38,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/website", websitesRouter.router);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -52,14 +54,14 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(PORT, () => {
-  log(chalk.bgGreenBright(`Server up at ${PORT}`));
+  log(chalk.cyan(`Server up at ${PORT}`));
 
   MongoClient.connect(
     DB_URL,
-    function (err, client) {
+    function(err, client) {
       if (err) throw err;
 
-      log(chalk.bgGreenBright(`MongoDB up at ${DB_URL}`));
+      log(chalk.cyan(`MongoDB up at ${DB_URL}`));
 
       const db = client.db(DB_NAME);
 
--- routes/websites.js
+++ routes/websites.js
@@ -3,25 +3,41 @@ const fs = require("fs");
 const router = express.Router();
 const scrap = require("scrap");
 const path = require("path");
-const urlToImage = require('url-to-image');
-const jimp = require('jimp');
+const urlToImage = require("url-to-image");
+const jimp = require("jimp");
 
 let dbClient = null;
 
 router.get("/list", (req, res) => {
-  dbClient.find({}).toArray((err, data) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
+  dbClient
+    .collection("sites")
+    .find({})
+    .toArray((err, data) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
 
-    return res.status(200).json({ list: data });
-  });
+      return res.status(200).json({ list: data });
+    });
+});
+
+router.get("/categories", (req, res) => {
+  dbClient
+    .collection("categories")
+    .find({})
+    .toArray((err, data) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+
+      return res.status(200).json({ categories: data });
+    });
 });
 
 router.post("/create", async (req, res) => {
   // Recojo los params de la petición (cuerpo)
   const urlWeb = req.body.url;
-  const titleWeb = req.body.title;
+  const categoryWeb = req.body.category;
 
   const urlScrapped = scrap(urlWeb, async (err, $, code, html) => {
     if (err) {
@@ -33,28 +49,45 @@ router.post("/create", async (req, res) => {
     const titleScrap = $("title").html();
     // Buscamos entre los metas para encontrar el "Description"
     const $metas = $("meta");
-    const metaDescription = Object.keys($metas).filter(item => $metas[item].attribs && $metas[item].attribs.name && $metas[item].attribs.name.toLowerCase() === "description");
+    const metaDescription = Object.keys($metas).filter(
+      item =>
+        $metas[item].attribs &&
+        $metas[item].attribs.name &&
+        $metas[item].attribs.name.toLowerCase() === "description"
+    );
     const descriptionScrap = $metas[metaDescription].attribs.content;
 
     // Crear screenshot
     const imageName = `image-${new Date().getTime()}.png`;
     const pathToSave = path.resolve(__basepath, "public", "images", imageName);
-    const pathToSaveOptimized = path.resolve(__basepath, "public", "images", "optimized", imageName);
+    const pathToSaveOptimized = path.resolve(
+      __basepath,
+      "public",
+      "images",
+      "optimized",
+      imageName
+    );
     const imageUrl = `images/optimized/${imageName}`;
 
     await urlToImage(urlWeb, pathToSave).catch(err => res.status(500).send());
 
     const rawImage = await jimp.read(pathToSave);
 
-    await rawImage.resize(450, jimp.AUTO).crop(0, 0, 450, 300).quality(80).writeAsync(pathToSaveOptimized);
+    await rawImage
+      .resize(450, jimp.AUTO)
+      .crop(0, 0, 450, 300)
+      .quality(80)
+      .writeAsync(pathToSaveOptimized);
 
     // Creamos el documento
     const insertion = await dbClient
+      .collection("sites")
       .insertOne({
         url: urlWeb,
         title: titleScrap,
         description: descriptionScrap,
-        image: imageUrl
+        image: imageUrl,
+        category: categoryWeb
       })
       .catch(err => res.status(500).json({ error: err.message }));
 
@@ -73,11 +106,9 @@ router.post("/create", async (req, res) => {
       });
     });
   });
-
-
 });
 
 module.exports = {
   router,
-  setMongoClient: client => (dbClient = client.collection("sites"))
+  setMongoClient: client => (dbClient = client)
 };
